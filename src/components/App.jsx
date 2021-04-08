560aa45af4e0e5fd7effda369c407481a9ac65f9
eksite <denisandipod@gmail.com>
2021-04-08 18:45:02 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,8 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import TrelloList from "./TrelloList.jsx";
-import { useListsState } from "../context/index.jsx";
-import TrelloActionButton from "./TrelloActionButton.jsx";
 
 const App = () => {
-  const state = useListsState();
-  return (
-    <>
-      <div style={styles.listsContainer}>
-        {state.lists.map((list) => (
-          <TrelloList
-            key={list.id}
-            listId={list.id}
-            title={list.title}
-            cards={list.cards}
-          />
-        ))}
-        <TrelloActionButton list />
-      </div>
-    </>
-  );
-};
-
-const styles = {
-  listsContainer: {
-    display: "flex",
-    flexDirection: "row",
-  },
+ return <div> smth interesting</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
-import { ListsProvider } from "./context/index.jsx";
 
-ReactDOM.render(
-  <ListsProvider>
-    <App />
-  </ListsProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
